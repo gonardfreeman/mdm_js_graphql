9132a2d8d798ea991345fdd4980cd9bbb64f7d90
Your Name <you@example.com>
2017-11-13 17:51:23 +0200

    first commit

---
+++ src/schema/server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var PORT = 4000;
+
+var schema = buildSchema(`
+    type Query {
+        quoteOfTheDay: String
+        random: Float!
+        rollThreeDice: [Int]
+    }
+`);
+
+var root = { 
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1,2,3].map(_ => 1 + Math.floor(Math.random() * 6));
+    }
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(PORT);
+console.log(`Running GraphQL API server at localhost:${PORT}/graphql`);
